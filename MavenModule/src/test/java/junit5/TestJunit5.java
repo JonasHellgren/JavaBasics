0bcaeeee0ce1303a569c19e220d820b264f9b9d9
Jonas Hellgren <jonas.hellgren@volvo.com>
2023-03-09 07:23:10 +0100

    TestElevatorRules

--- MavenModule/src/test/java/junit5/TestJunit5.java
+++ MavenModule/src/test/java/junit5/TestJunit5.java
@@ -7,8 +7,6 @@ import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.MethodSource;
-
-
 import java.util.Arrays;
 import java.util.List;
 
